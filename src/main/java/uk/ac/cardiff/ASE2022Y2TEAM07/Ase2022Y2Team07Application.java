37f65f4d6b0bd7ef542aef8bf20621fba4577930
Toby Nott <tobynott80@gmail.com>
2022-11-21 23:03:44 +0000

    Initialised Spring Framework

---
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/Ase2022Y2Team07Application.java
@@ -0,0 +1,13 @@
+package uk.ac.cardiff.ASE2022Y2TEAM07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ase2022Y2Team07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ase2022Y2Team07Application.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/cardiff/ASE2022Y2TEAM07/Ase2022Y2Team07ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cardiff.ASE2022Y2TEAM07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ase2022Y2Team07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
