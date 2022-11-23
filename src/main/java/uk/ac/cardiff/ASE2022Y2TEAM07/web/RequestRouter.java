25d622d0bd593b51d8678d5f97b67c52bf106f78
Nathaniel Bassett <BassettN1@cardiff.ac.uk>
2022-11-23 14:24:00 +0000

    fixed issues on html not loading and css not connecting and mvc

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RequestRouter implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/LoginPage").setViewName("login");
+        registry.addViewController("login").setViewName("/LoginPage");
     }
 
 }
