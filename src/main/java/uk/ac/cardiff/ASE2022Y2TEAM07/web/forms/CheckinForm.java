e23361a322095d7abf30ee9755c41b0baceffa43
Toby Nott <tobynott80@gmail.com>
2022-11-23 09:56:27 +0000

    added checkin routes and required classes

---
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/forms/CheckinForm.java
@@ -0,0 +1,11 @@
+package uk.ac.cardiff.ASE2022Y2TEAM07.web.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CheckinForm {
+    private Integer employeeID;
+    private Integer score;
+}
--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/forms/EmployeeSignupForm.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/forms/EmployeeSignupForm.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-
 public class EmployeeSignupForm {
     private String name;
     private String email;
