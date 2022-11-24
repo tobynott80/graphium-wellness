23c8ae578c5d8c204398fd33a217e188018d9c40
Tobiah Nott <notttj@cardiff.ac.uk>
2022-11-24 14:47:51 +0000

    added one to one request page and routing

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
@@ -10,6 +10,8 @@ public class RequestRouter implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("login").setViewName("/LoginPage");
         registry.addViewController("/employee/checkin").setViewName("/EmployeeCheckinPage");
+
+        registry.addViewController("/employee/onetoone").setViewName("/EmployeeOneToOnePage");
     }
 
 }
