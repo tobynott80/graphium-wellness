53cf17046bf93d3ed06a7e5f6c9d0d6fefda3d61
Nathaniel Bassett <BassettN1@cardiff.ac.uk>
2022-11-25 18:38:40 +0000

    added EmployeeHomepage.html, style sheet for it, homepageGraph.js, and path to RequestRouter

---
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
@@ -0,0 +1,14 @@
+package uk.ac.cardiff.ASE2022Y2TEAM07.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RequestRouter implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("homepage").setViewName("/EmployeeHomepage");
+    }
+
+}
