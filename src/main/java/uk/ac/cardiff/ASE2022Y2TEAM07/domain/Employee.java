c76dc2428729ce8813d9a666e2f0c76c5add728d
Tobiah Nott <notttj@cardiff.ac.uk>
2022-11-28 21:22:54 +0000

    fixed domain classes

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/domain/Employee.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/domain/Employee.java
@@ -2,14 +2,42 @@ package uk.ac.cardiff.ASE2022Y2TEAM07.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
+@Table
 public class Employee {
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public int getSupervisorId() {
+        return supervisorId;
+    }
+
     private int employeeId;
     private String name;
     private String email;
     private String role;
     private String passwordHash;
     private int supervisorId;
+
+
 }
