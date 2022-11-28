77f99d28761928ca9fac73ed04657c40f32b6b92
c21049134 <ghawid@cardiff.ac.uk>
2022-11-28 13:20:13 +0000

    Fixed H2 connection, Edited the RequestRouter.

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/RequestRouter.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RequestRouter implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("homepage").setViewName("/EmployeeHomepage");
-        registry.addViewController("login").setViewName("/LoginPage");
+        registry.addViewController("/homepage").setViewName("/EmployeeHomepage");
+        registry.addViewController("/login").setViewName("/LoginPage");
         registry.addViewController("/supervisor/onetone").setViewName("/SupervisorOneToOne");
-        registry.addViewController("supervisor").setViewName("/SupervisorPage");
+        registry.addViewController("/supervisor").setViewName("/SupervisorPage");
         registry.addViewController("/employee/checkin").setViewName("/EmployeeCheckinPage");
         registry.addViewController("/employee/onetoone").setViewName("/EmployeeOneToOnePage");
         registry.addViewController("/404").setViewName("error/404");
