e9a7ee9ee17c8e06a17440a954ac6f906da3fa20
Nathaniel Bassett <BassettN1@cardiff.ac.uk>
2022-11-29 15:13:47 +0000

    OneToOneServiceImpl implements OneToOneService

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/service/OneToOneServiceImpl.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/service/OneToOneServiceImpl.java
@@ -2,8 +2,15 @@ package uk.ac.cardiff.ASE2022Y2TEAM07.service;
 
 
 import org.springframework.stereotype.Service;
+import uk.ac.cardiff.ASE2022Y2TEAM07.dto.OneToOneDto;
+
+import java.util.List;
 
 @Service
-public class OneToOneServiceImpl {
+public class OneToOneServiceImpl implements OneToOneService {
+    @Override
+    public List<OneToOneDto> findAll() {
+        return null;
+    }
     //TODO: Implement this service
 }
