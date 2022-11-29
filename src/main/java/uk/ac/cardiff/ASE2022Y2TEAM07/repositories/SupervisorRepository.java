965cfe7bf7d1a97a938c9db8d9cce63c294e00df
Tobiah Nott <notttj@cardiff.ac.uk>
2022-11-29 14:06:22 +0000

    implemented all the SupervisorRepo classes

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/repositories/SupervisorRepository.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/repositories/SupervisorRepository.java
@@ -2,6 +2,12 @@ package uk.ac.cardiff.ASE2022Y2TEAM07.repositories;
 
 import uk.ac.cardiff.ASE2022Y2TEAM07.domain.Supervisor;
 
+import java.util.List;
+
 public interface SupervisorRepository {
+    Supervisor findBySupervisorId(Integer supervisorId);
+
     Supervisor findByEmail(String email);
+
+    List<Supervisor> findAll();
 }
--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/repositories/SupervisorRepositoryImpl.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/repositories/SupervisorRepositoryImpl.java
@@ -4,13 +4,32 @@ package uk.ac.cardiff.ASE2022Y2TEAM07.repositories;
 import org.springframework.stereotype.Repository;
 import uk.ac.cardiff.ASE2022Y2TEAM07.domain.Supervisor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class SupervisorRepositoryImpl implements SupervisorRepository {
 
+    private SupervisorRepositorySpringDataJdbc repoJdbc;
+
+    public SupervisorRepositoryImpl(SupervisorRepositorySpringDataJdbc aRepo) {
+        repoJdbc = aRepo;
+    }
 
     @Override
+    public Supervisor findBySupervisorId(Integer supervisorId) {
+        return repoJdbc.findBySupervisorId(supervisorId);
+    }
+    @Override
     public Supervisor findByEmail(String email) {
-        return null;
+        return repoJdbc.findByEmail(email);
+    }
+
+    @Override
+    public List<Supervisor> findAll() {
+        List<Supervisor> supervisors = new ArrayList<>();
+        repoJdbc.findAll().forEach(supervisors::add);
+        return supervisors;
     }
 }
 
