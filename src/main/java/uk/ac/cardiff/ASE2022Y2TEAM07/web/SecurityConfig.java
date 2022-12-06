40724df7e0cef77d69c97c6ab0647b6f4776ffc9
Tobiah Nott <notttj@cardiff.ac.uk>
2022-12-06 13:17:34 +0000

    added exception in spring security config for h2console

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 //adapted from: https://stackoverflow.com/questions/49387988/authentication-of-users-from-two-database-tables-in-spring-security
 
 @Configuration
