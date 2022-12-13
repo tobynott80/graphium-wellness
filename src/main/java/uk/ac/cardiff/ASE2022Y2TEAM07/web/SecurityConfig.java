33b45b41f29d937a6444d8df310cf78ac8d40aee
Tobiah Nott <notttj@cardiff.ac.uk>
2022-12-13 18:19:13 +0000

    added bcrypt encoding to check against stored hashed

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,13 +20,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//
-//    @Bean
-//    @Order(1)
-//    public SecurityFilterChain h2FilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests().requestMatchers("/h2-console/**").permitAll();
-//        return http.build();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -52,9 +51,7 @@ public class SecurityConfig {
     public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
-        // TODO: Low Priority: Replace NoOpPasswordEncoder with BCryptPasswordEncoder
-        // This assumes passwords are in plain text - THEY WON'T BE IN PRODUCTION!! - This should definitely be changed to BCrypt
-        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
 
