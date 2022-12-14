9fb2df786d26592445a460abb4990d7dc9dedd8e
Tobiah Nott <notttj@cardiff.ac.uk>
2022-12-14 15:12:48 +0000

    added lombok no args constructor

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/forms/OneToOneForm.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/forms/OneToOneForm.java
@@ -5,13 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class OneToOneForm {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -19,24 +17,4 @@ public class OneToOneForm {
 
     private String notes;
 
-    public OneToOneForm() {
-        this.date = LocalDate.now();
-        this.notes = "";
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
