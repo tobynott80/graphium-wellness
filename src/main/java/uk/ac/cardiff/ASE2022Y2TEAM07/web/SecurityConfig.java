b502a57f7ba2127ddc5312c81d875ff02b99349c
Tobiah Nott <notttj@cardiff.ac.uk>
2022-12-28 19:22:27 +0000

    added necessary security annotaion to only allow supervisors to view supervisor page

--- src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
+++ src/main/java/uk/ac/cardiff/ASE2022Y2TEAM07/web/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
